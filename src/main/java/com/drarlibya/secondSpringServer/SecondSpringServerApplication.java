5a5594182d8f3fb5a91a2bf55d09962b43ece71c
Drar <zzxxccz908@gmail.com>
2021-06-27 01:53:06 +0200

    Initial commit

---
+++ src/main/java/com/drarlibya/secondSpringServer/SecondSpringServerApplication.java
@@ -0,0 +1,13 @@
+package com.drarlibya.secondSpringServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSpringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondSpringServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drarlibya/secondSpringServer/SecondSpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drarlibya.secondSpringServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondSpringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
